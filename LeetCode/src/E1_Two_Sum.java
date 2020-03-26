e203607b748b1d440641f8837bed23a81cbd8fa7
TA-YU Mar <txm986@cs.bham.ac.uk>
2020-03-26 12:22:23 +0000

    Update the hashmap method.

--- LeetCode/src/E1_Two_Sum.java
+++ LeetCode/src/E1_Two_Sum.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of integers, return indices of the two numbers such that they
  * add up to a specific target.
@@ -14,12 +17,47 @@
 
 public class E1_Two_Sum {
 
+    /**
+     * Use hash map to find out the two sum.
+     * 
+     * @param nums   The input of integer array.
+     * @param target The target value of two sum.
+     * @return The integer array for the position.
+     */
+    public int[] twoSum_map(int[] nums, int target) {
+
+        // Create a hash map to store array value and position.
+        Map<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (hashMap.containsKey(complement)) {
+                // return the previous position in hashmap and i for now.
+                return new int[] { hashMap.get(complement), i };
+            }
+            hashMap.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum.");
+    }
+
+    /**
+     * Use for loop to find the two sum.
+     * 
+     * @param nums   The input of integer array.
+     * @param target The target value of two sum.
+     * @return The integer array for the position.
+     */
     public int[] twoSum(int[] nums, int target) {
-        
-        for(int i = 0; i < nums.length - 1; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(target - nums[i] == nums[j]){
-                    return new int[]{i ,j};
+
+        /*
+         * If there still cannot find the solution at nums.length - 1, no need to keep
+         * find the next value.
+         */
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[] { i, j };
                 }
             }
         }
@@ -30,10 +68,17 @@ public class E1_Two_Sum {
     public static void main(String[] args) {
         E1_Two_Sum solution = new E1_Two_Sum();
         int[] nums = new int[] {2,7,11,15};
-        int target = 17;
+        int target = 26;
         
         try {
             int[] ans = solution.twoSum(nums, target);
+            System.out.println("[" + ans[0] + ", " + ans[1] + "]");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        try {
+            int[] ans = solution.twoSum_map(nums, target);
             System.out.println("[" + ans[0] + ", " + ans[1] + "]");
         } catch (Exception e) {
             System.out.println(e.toString());
