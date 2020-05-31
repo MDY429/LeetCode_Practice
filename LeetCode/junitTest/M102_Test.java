82af3663dfbc66aa6f2b3d30401a5ea00763539c
MDY429 <michael6229@gmail.com>
2020-05-31 17:01:00 +0100

    Update Binary Tree Level Order Traversal code and JUnit.

---
+++ LeetCode/junitTest/M102_Test.java
@@ -0,0 +1,111 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Use JUnit to make the verify.
+ * 
+ * @author MDY
+ */
+public class M102_Test {
+    M102_Binary_Tree_Level_Order_Traversal m102;
+    TreeNode t1, t2, t3, t4, t5;
+
+    @BeforeEach
+    public void init() {
+        m102 = new M102_Binary_Tree_Level_Order_Traversal();
+
+        Integer[] a = new Integer[] { 3, 9, 20, 15, 7, 10, null, null, 3, 8, 1, 13 };
+        t1 = TreeNode.build(a);
+
+        Integer[] b = new Integer[] { 1, 2, null, 3, null, null, null, 4, null, null, null, null, null, null, null, 5 };
+        t2 = TreeNode.build(b);
+
+        Integer[] c = new Integer[] { 5, null, 4, null, null, null, 3, null, null, null, null, null, null, null, 2,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 1 };
+        t3 = TreeNode.build(c);
+
+        Integer[] d = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        t4 = TreeNode.build(d);
+
+        Integer[] e = new Integer[] {};
+        t5 = TreeNode.build(e);
+    }
+
+    @Test
+    public void M102_test1() {
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(3));
+        expected.add(Arrays.asList(9, 20));
+        expected.add(Arrays.asList(15, 7, 10));
+        expected.add(Arrays.asList(3, 8, 1, 13));
+
+        List<List<Integer>> actual = m102.levelOrder(t1);
+        assertEquals(expected, actual);
+
+        List<List<Integer>> actual2 = m102.levelOrder_dfs_iterator(t1);
+        assertEquals(expected, actual2);
+    }
+
+    @Test
+    public void M102_test2() {
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(1));
+        expected.add(Arrays.asList(2));
+        expected.add(Arrays.asList(3));
+        expected.add(Arrays.asList(4));
+        expected.add(Arrays.asList(5));
+
+        List<List<Integer>> actual = m102.levelOrder(t2);
+        assertEquals(expected, actual);
+
+        List<List<Integer>> actual2 = m102.levelOrder_dfs_iterator(t2);
+        assertEquals(expected, actual2);
+    }
+
+    @Test
+    public void M102_test3() {
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(5));
+        expected.add(Arrays.asList(4));
+        expected.add(Arrays.asList(3));
+        expected.add(Arrays.asList(2));
+        expected.add(Arrays.asList(1));
+
+        List<List<Integer>> actual = m102.levelOrder(t3);
+        assertEquals(expected, actual);
+
+        List<List<Integer>> actual2 = m102.levelOrder_dfs_iterator(t3);
+        assertEquals(expected, actual2);
+    }
+
+    @Test
+    public void M102_test4() {
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(1));
+        expected.add(Arrays.asList(2, 3));
+        expected.add(Arrays.asList(4, 5, 6, 7));
+        expected.add(Arrays.asList(8, 9, 10, 11, 12, 13, 14, 15));
+
+        List<List<Integer>> actual = m102.levelOrder(t4);
+        assertEquals(expected, actual);
+
+        List<List<Integer>> actual2 = m102.levelOrder_dfs_iterator(t4);
+        assertEquals(expected, actual2);
+    }
+
+    @Test
+    public void M102_test5() {
+        List<List<Integer>> expected = new ArrayList<>();
+        
+        List<List<Integer>> actual = m102.levelOrder(t5);
+        assertEquals(expected, actual);
+
+        List<List<Integer>> actual2 = m102.levelOrder_dfs_iterator(t5);
+        assertEquals(expected, actual2);
+    }
+}
---
+++ LeetCode/src/M102_Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,123 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * url: 
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * 
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its level order traversal as:
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ */
+public class M102_Binary_Tree_Level_Order_Traversal {
+    
+    /**
+     * Use dfs recursion to find every level's value.
+     * 
+     * @param root The input of Tree Node.
+     * @return Every level elements.
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        dfs(root, ans, 0);
+
+        return ans;
+    }
+
+    /**
+     * Use recursion and dfs to traverse every node and put value into corresponding
+     * list.
+     * 
+     * @param root  The input of Tree Node.
+     * @param ans   The answer list.
+     * @param index The level.
+     */
+    private void dfs(TreeNode root, List<List<Integer>> ans, int index) {
+
+        if (root == null) {
+            return;
+        }
+
+        // If there is no this level's elements list.
+        if (ans.size() < index + 1) {
+            ans.add(new ArrayList<>());
+        }
+
+        ans.get(index).add(root.val);
+
+        dfs(root.left, ans, index + 1);
+        dfs(root.right, ans, index + 1);
+
+    }
+
+    /**
+     * Use iterator and dfs to find the each level's elements.
+     * 
+     * @param root he input of Tree Node.
+     * @return Every level elements.
+     */
+    public List<List<Integer>> levelOrder_dfs_iterator(TreeNode root) {
+
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        q.add(root);
+        q.add(null);    // Set a null break to know the level is end.
+        List<Integer> tmp = new ArrayList<>();
+
+        while (q.size() > 1) {
+
+            TreeNode node = q.poll();
+
+            if (node == null) {
+                ans.add(tmp);
+                tmp = new ArrayList<>();
+                q.add(null);    // Set a null break to know the level is end.
+            } else {
+                tmp.add(node.val);
+
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+        }
+
+        ans.add(tmp);
+
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        M102_Binary_Tree_Level_Order_Traversal m102 = new M102_Binary_Tree_Level_Order_Traversal();
+
+        Integer[] arr = new Integer[] { 3, 9, 20, null, null, 15, 7 };
+        TreeNode root = TreeNode.build(arr);
+
+        System.out.println(m102.levelOrder(root));
+    }
+}
